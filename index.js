b935dbd4277a0c0454b780adb15b66358aad3951
Lasse Haverinen <lasse.haverinen@gmail.com>
2016-08-31 17:25:22 +0300

    Added /api prefix to routes. Added static book data. Added route for single book.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var books = require('./routes/books.js');
 var app = express();
 
 app.set('port', (process.env.PORT || 80));
+app.use(bodyParser.json());
 
-app.use('/books', books);
+app.use('/api', books);
 
 app.get('/', function(req, res) {
     res.send("Hello world");
--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
-router.route('/')
+var bookData = [{name: "test book 1",
+            author: 25},
+            {name: "test book 2",
+            author: 445}];
+
+function getAllBooks(req,res,next)
+{
+    res.json(bookData);
+}
+
+function newBook(req,res,next)
+{
+    res.sendStatus(200);
+}
+
+router.route('/books')
+    .get(getAllBooks)
+    .post(newBook);
+
+
+router.route('/books/:id')
     .get(function(req,res,next){
-        res.send("get books");
+        
+        console.log(req.params);
+        res.send("request book id " + req.params.id);
     })
     .post(function(req,res,next){
         res.send("post books");
@@ -12,6 +34,6 @@ router.route('/')
         res.send("delete books");
     });
 
-  
+ 
 
 module.exports = router;
