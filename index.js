58040bd43a6953564b253d06e8d2c6b1a1300360
Lasse Haverinen <lasse.haverinen@gmail.com>
2016-08-31 15:02:12 +0300

    Test route added

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get('/', function(req, res) {
 
 });
 
+
+app.get('/test', function(req, res) {
+    res.send("Test world");
+
+});
+
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
